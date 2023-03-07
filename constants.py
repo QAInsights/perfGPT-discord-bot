f5847b19d4dc0940ed2ed572bc428938f6295bad
NaveenKumar Namachivayam ⚡ <catch.nkn@gmail.com>
2023-03-06 22:46:46 -0500

    Add: read encrypted tokens
    Signed-off-by: NaveenKumar Namachivayam ⚡ <catch.nkn@gmail.com>

--- constants.py
+++ constants.py
@@ -1,4 +1,6 @@
+DISCORD_GUILD = "PerfGPT"
 openai_model = "gpt-3.5-turbo"
 initial_conversation = "You are a helpful assistant."
 emoji_name = "spinner"
-DISCORD_GUILD = "PerfGPT"
+secret_name = "DISCORD_BOT"
+region_name = "us-east-2"
--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@ import json
 import boto3
 from botocore.exceptions import ClientError
 
+import constants
+
 
 def pop_conversation(previous_conversation_response=None):
     return previous_conversation_response.pop(0)
@@ -14,11 +16,11 @@ def get_secret(secret_key):
     :param secret_key:  secret key name
     :return:            decrypted secrets
     """
-    secret_name = "DISCORD_BOT"
-    region_name = "us-east-2"
+    secret_name = constants.secret_name
+    region_name = constants.region_name
 
     # Create a Secrets Manager client
-    session = boto3.Session(profile_name='awscli')
+    session = boto3.session.Session()
     client = session.client(
         service_name='secretsmanager',
         region_name=region_name
